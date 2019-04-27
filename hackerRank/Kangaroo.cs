f8d7246701bc0e22d041bb2034f16463740118ed
riswit <dino.solano@riswit.it>
2019-04-27 23:18:04 +0200

    Kangaroo

--- hackerRank/Kangaroo.cs
+++ hackerRank/Kangaroo.cs
@@ -8,5 +8,41 @@ namespace hackerRank
 {
     class Kangaroo
     {
+        public void Execute()
+        {
+            int x1 = 0;
+            int v1 = 3;
+            int x2 = 4;
+            int v2 = 2;
+
+            String can = matchKangaroo(x1, v1, x2, v2);
+
+            Console.WriteLine(can);
+        }
+
+        static string matchKangaroo(int x1, int v1, int x2, int v2)
+        {
+            int len1 = v1 - x1;
+
+            if ((x2 >= x1 && v2 > v1) 
+                || (x1 >= x2 && v1 > v2) 
+                || (x2 >= v1 && v2 > v1) 
+                || (x2 > len1 && v2 > v1))
+            {
+                return "NO";
+            }
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                if (x1 + v1 == x2 + v2) {
+                    return "YES";
+                }
+                x1 += v1;
+                x2 += v2;
+            }
+
+            return "NO";
+        }
+
     }
 }
--- hackerRank/Program.cs
+++ hackerRank/Program.cs
@@ -13,8 +13,11 @@ namespace hackerRank
             //GradingStudents gradingStudents = new GradingStudents();
             //gradingStudents.Execute();
 
-            AppleAndOrange appleAndOrange = new AppleAndOrange();
-            appleAndOrange.Execute();
+            //AppleAndOrange appleAndOrange = new AppleAndOrange();
+            //appleAndOrange.Execute();
+
+            Kangaroo kangaroo = new Kangaroo();
+            kangaroo.Execute();
 
             Console.WriteLine("Press any key to continue ...");
             Console.ReadKey();
