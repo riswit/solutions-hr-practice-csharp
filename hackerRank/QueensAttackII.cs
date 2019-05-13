93039f5c92ae72e932bde14d195e19be97d5bc12
riswit <dino.solano@riswit.it>
2019-05-13 23:31:38 +0200

    QueensAttackII without obstacles

--- hackerRank/QueensAttackII.cs
+++ hackerRank/QueensAttackII.cs
@@ -10,16 +10,40 @@ namespace hackerRank
     {
         public void Execute()
         {
-            int n = 5;
-            int k = 3;
+            int n = 9;
+            int k = 0;
             int r_q = 4;
-            int c_q = 3;
+            int c_q = 4;
             int[][] obstacles = new int[][] {
                 new int[] { 5, 5 },
                 new int[] { 4, 2 },
                 new int[] { 2, 3 }
             };
-            int resExp = 10;
+            int resExp = 27;
+
+            //int n = 5;
+            //int k = 3;
+            //int r_q = 4;
+            //int c_q = 3;
+            //int[][] obstacles = new int[][] {
+            //    new int[] { 5, 5 },
+            //    new int[] { 4, 2 },
+            //    new int[] { 2, 3 }
+            //};
+            //int resExp = 10;
+
+            //int n = 4;
+            //int k = 0;
+            //int r_q = 4;
+            //int c_q = 4;
+            //int[][] obstacles = new int[][] {
+            //    new int[] { 1, 5 },
+            //    new int[] { 2, 4 },
+            //    new int[] { 3, 3 },
+            //    new int[] { 4, 2 },
+            //    new int[] { 5, 1 }
+            //};
+            //int resExp = 9;
 
             int result = queensAttack(n, k, r_q, c_q, obstacles);
 
@@ -56,15 +80,15 @@ namespace hackerRank
             }
 
             totSquares = calcTop(n, k, rowQ, colQ, obstacles);
-            //totSquares = calcTopLeft(n, k, rowQ, colQ, obstacles);
-            //totSquares = calcTopRight(n, k, rowQ, colQ, obstacles);
+            totSquares += calcTopLeft(n, k, rowQ, colQ, obstacles);
+            totSquares += calcTopRight(n, k, rowQ, colQ, obstacles);
 
-            //totSquares += calcBottom(n, k, rowQ, colQ, obstacles);
-            //totSquares += calcBottomLeft(n, k, rowQ, colQ, obstacles);
-            //totSquares += calcBottomRight(n, k, rowQ, colQ, obstacles);
+            totSquares += calcBottom(n, k, rowQ, colQ, obstacles);
+            totSquares += calcBottomLeft(n, k, rowQ, colQ, obstacles);
+            totSquares += calcBottomRight(n, k, rowQ, colQ, obstacles);
 
-            //totSquares += calcLeft(n, k, rowQ, colQ, obstacles);
-            //totSquares += calcRight(n, k, rowQ, colQ, obstacles);
+            totSquares += calcLeft(n, k, rowQ, colQ, obstacles);
+            totSquares += calcRight(n, k, rowQ, colQ, obstacles);
 
             return totSquares;
         }
@@ -76,68 +100,236 @@ namespace hackerRank
                 return 0;
             }
             int totSquares = 0;
-            int obstacle = 0;
-            int squares = 0;
-            int rowObs = 0;
+            int[] s;
 
             if (k > 0)
             {
-                //int maxValue = Array.FindIndex(  obstacles.Max();
-                //int maxIndex = obstacles.ToList().IndexOf(maxValue);
+                s = obstacles.Where((row, index) => row[0] < rowQ && row[1] == colQ).Max();
+                totSquares += ((rowQ - s[0]) - 1);
+            }
+            else
+            {
+                totSquares += rowQ;
             }
 
             return totSquares;
         }
 
-        static int sumLines(int[][] s, int line)
+        static int calcBottom(int n, int k, int rowQ, int colQ, int[][] obstacles)
         {
-            int sum = 0;
-
-            for (int col = 0; col < s.Length; col++)
+            if (rowQ == (n - 1))
             {
-                sum += s[line][col];
+                return 0;
+            }
+            int totSquares = 0;
+            int[] s;
+
+            if (k > 0)
+            {
+                s = obstacles.Where((row, index) => row[0] > rowQ && row[1] == colQ).Min();
+                totSquares += ((s[0] - rowQ) - 1);
+            }
+            else
+            {
+                totSquares += ((n - 1) - rowQ);
             }
 
-            return sum;
+            return totSquares;
         }
 
-        static int sumCols(int[][] s, int col)
+        static int calcLeft(int n, int k, int rowQ, int colQ, int[][] obstacles)
         {
-            int sum = 0;
-
-            for (int line = 0; line < s.Length; line++)
+            if (colQ == 0)
             {
-                sum += s[line][col];
+                return 0;
+            }
+            int totSquares = 0;
+            int[] s;
+
+            if (k > 0)
+            {
+                s = obstacles.Where((row, index) => row[0] == rowQ && row[1] < colQ).Max();
+                totSquares += ((colQ - s[0]) - 1);
+            }
+            else
+            {
+                totSquares += colQ;
             }
 
-            return sum;
+            return totSquares;
         }
 
-        static int sumDiag(int[][] s, string d = "sx")
+        static int calcRight(int n, int k, int rowQ, int colQ, int[][] obstacles)
         {
-            int sum = 0;
-
-            switch (d)
+            if (colQ == (n - 1))
             {
-                case "sx":
-                    for (int i = 0; i < s.Length; i++)
+                return 0;
+            }
+            int totSquares = 0;
+            int[] s;
+
+            if (k > 0)
+            {
+                s = obstacles.Where((row, index) => row[0] == rowQ && row[1] > colQ).Min();
+                totSquares += ((s[0] - colQ) - 1);
+            }
+            else
+            {
+                totSquares += ((n - 1) - colQ);
+            }
+
+            return totSquares;
+        }
+
+        static int calcTopLeft(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        {
+            if (rowQ == 0 || colQ == 0)
+            {
+                return 0;
+            }
+            int totSquares = 0;
+            int[] s;
+
+            int diff = Math.Abs(colQ - rowQ);
+
+            if (k > 0)
+            {
+                s = obstacles.Where((row, index) => row[0] < rowQ && row[1] < colQ 
+                                        && row[0] == row[1] + diff)
+                                        .Max();
+
+                totSquares += ((rowQ - s[0]) - 1);
+            }
+            else
+            {
+                if (rowQ > colQ)
+                {
+                    totSquares += (rowQ - diff);
+                }
+                else
+                {
+                    if (rowQ < colQ)
                     {
-                        sum += s[i][i];
+                        totSquares += (colQ - diff);
                     }
-                    break;
-                case "dx":
-                    int col = s.Length - 1;
-                    for (int i = 0; i < s.Length; i++)
+                    else
                     {
-                        sum += s[i][col];
-                        col--;
+                        totSquares += rowQ;
                     }
-                    break;
+                }
             }
 
-            return sum;
+            return totSquares;
         }
 
+        static int calcTopRight(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        {
+            if (rowQ == 0 || colQ == (n - 1))
+            {
+                return 0;
+            }
+            int totSquares = 0;
+            int[] s;
+
+            int diff = Math.Abs(colQ - rowQ);
+            int diffC = 0;
+            int diffR = 0;
+
+            if (k > 0)
+            {
+                s = obstacles.Where((row, index) => row[0] < rowQ && row[1] > colQ 
+                                        && row[0] == row[1] + diff)
+                                        .Min();
+                totSquares += ((rowQ - s[0]) - 1);
+            }
+            else
+            {
+                diffC = (n - 1) - colQ;
+                if (rowQ < diffC)
+                {
+                    totSquares += rowQ;
+                }
+                else
+                {
+                    totSquares += diffC;
+                }
+            }
+
+            return totSquares;
+        }
+
+        static int calcBottomLeft(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        {
+            if (rowQ == (n - 1) || colQ == 0)
+            {
+                return 0;
+            }
+            int totSquares = 0;
+            int[] s;
+
+            int diff = Math.Abs(colQ - rowQ);
+            int diffC = 0;
+            int diffR = 0;
+
+            if (k > 0)
+            {
+                s = obstacles.Where((row, index) => row[0] > rowQ && row[1] < colQ
+                                        && row[0] == row[1] + diff)
+                                        .Min();
+                totSquares += ((rowQ - s[0]) - 1);
+            }
+            else
+            {
+                diffR = (n - 1) - rowQ;
+                if (colQ < diffR)
+                {
+                    totSquares += rowQ;
+                }
+                else
+                {
+                    totSquares += diffR;
+                }
+            }
+
+            return totSquares;
+        }
+
+        static int calcBottomRight(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        {
+            if (rowQ == (n - 1) || colQ == (n - 1))
+            {
+                return 0;
+            }
+            int totSquares = 0;
+            int[] s;
+
+            int diff = Math.Abs(colQ - rowQ);
+            int diffC = 0;
+            int diffR = 0;
+
+            if (k > 0)
+            {
+                s = obstacles.Where((row, index) => row[0] > rowQ && row[1] > colQ
+                                        && row[0] == row[1] + diff)
+                                        .Min();
+                totSquares += ((rowQ - s[0]) - 1);
+            }
+            else
+            {
+                diffC = (n - 1) - colQ;
+                diffR = (n - 1) - rowQ;
+                if (diffR < diffC)
+                {
+                    totSquares += diffR;
+                }
+                else
+                {
+                    totSquares += diffC;
+                }
+            }
+
+            return totSquares;
+        }
 
     }
 }
