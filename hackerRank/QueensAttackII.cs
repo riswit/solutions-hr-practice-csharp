4154f42df0d70c23f7e392ab683c216e2d384a03
riswit <dino.solano@riswit.it>
2019-05-15 20:19:44 +0200

    Queens Attack II completed

--- hackerRank/QueensAttackII.cs
+++ hackerRank/QueensAttackII.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace hackerRank
 {
@@ -16,22 +17,21 @@ namespace hackerRank
             int c_q = 5;
             int[][] obstacles = new int[][] {
                 new int[] { 8, 1 },
-                new int[] { 8, 2 },
                 new int[] { 8, 5 },
                 new int[] { 7, 2 },
-                new int[] { 7, 8 },
                 new int[] { 6, 1 },
                 new int[] { 6, 5 },
                 new int[] { 6, 6 },
+                new int[] { 2, 7 },
+                new int[] { 4, 7 },
                 new int[] { 6, 7 },
                 new int[] { 5, 7 },
-                new int[] { 4, 2 },
-                new int[] { 4, 7 },
+                new int[] { 2, 6 },
                 new int[] { 3, 2 },
                 new int[] { 2, 4 },
+                new int[] { 4, 2 },
+                new int[] { 8, 2 },
                 new int[] { 2, 5 },
-                new int[] { 2, 6 },
-                new int[] { 2, 7 },
                 new int[] { 2, 8 },
                 new int[] { 1, 2 }
             };
@@ -86,9 +86,6 @@ namespace hackerRank
             Console.WriteLine(result);
         }
 
-        static int G_rowQ = 0;
-        static int G_colQ = 0;
-
         static int queensAttack(int n, int k, int r_q, int c_q, int[][] obstacles)
         {
             if (n == 1)
@@ -100,28 +97,32 @@ namespace hackerRank
             int rowQ = cRow(n, r_q);
             int colQ = cCol(c_q);
 
-            G_rowQ = rowQ;
-            G_colQ = colQ;
-
+            DataTable dt = new DataTable();
             if (k > 0)
             {
+                dt.Columns.Add("r", typeof(int));
+                dt.Columns.Add("c", typeof(int));
+                DataRow newRow;
+
                 for (int r = 0; r < obstacles.Length; r++)
                 {
-                    obstacles[r][0] = n - obstacles[r][0];
-                    obstacles[r][1] = obstacles[r][1] - 1;
+                    newRow = dt.NewRow();
+                    newRow[0] = cRow(n, obstacles[r][0]);
+                    newRow[1] = cCol(obstacles[r][1]);
+                    dt.Rows.Add(newRow);
                 }
             }
 
-            totSquares = calcTop(n, k, rowQ, colQ, obstacles);
-            totSquares += calcBottom(n, k, rowQ, colQ, obstacles);
-            totSquares += calcLeft(n, k, rowQ, colQ, obstacles);
-            totSquares += calcRight(n, k, rowQ, colQ, obstacles);
+            totSquares = calcTop(n, k, rowQ, colQ, dt);
+            totSquares += calcBottom(n, k, rowQ, colQ, dt);
+            totSquares += calcLeft(n, k, rowQ, colQ, dt);
+            totSquares += calcRight(n, k, rowQ, colQ, dt);
 
-            totSquares += calcTopLeft(n, k, rowQ, colQ, obstacles);
-            totSquares += calcTopRight(n, k, rowQ, colQ, obstacles);
+            totSquares += calcTopLeft(n, k, rowQ, colQ, dt);
+            totSquares += calcTopRight(n, k, rowQ, colQ, dt);
 
-            totSquares += calcBottomLeft(n, k, rowQ, colQ, obstacles);
-            totSquares += calcBottomRight(n, k, rowQ, colQ, obstacles);
+            totSquares += calcBottomLeft(n, k, rowQ, colQ, dt);
+            totSquares += calcBottomRight(n, k, rowQ, colQ, dt);
 
 
             return totSquares;
@@ -137,7 +138,7 @@ namespace hackerRank
             return val - 1;
         }
 
-        static int calcTop(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        static int calcTop(int n, int k, int rowQ, int colQ, DataTable dt)
         {
             if (rowQ == 0)
             {
@@ -148,10 +149,10 @@ namespace hackerRank
 
             if (k > 0)
             {
-                s = Array.Find(obstacles, predTop);
-                if (s != null)
+                DataRow[] foundRows = dt.Select("r < " + rowQ + " and c = " + colQ, "r desc");
+                if (foundRows.Length > 0)
                 {
-                    totSquares += ((rowQ - s[0]) - 1);
+                    totSquares += ((rowQ - int.Parse(foundRows[0][0].ToString())) - 1);
                     return totSquares;
                 }
             }
@@ -161,12 +162,7 @@ namespace hackerRank
             return totSquares;
         }
 
-        static bool predTop(int[] e)
-        {
-            return e[0] < G_rowQ && e[1] == G_colQ;
-        }
-
-        static int calcBottom(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        static int calcBottom(int n, int k, int rowQ, int colQ, DataTable dt)
         {
             if (rowQ == (n - 1))
             {
@@ -177,10 +173,10 @@ namespace hackerRank
 
             if (k > 0)
             {
-                s = Array.Find(obstacles, predBottom);
-                if (s != null)
+                DataRow[] foundRows = dt.Select("r > " + rowQ + " and c = " + colQ, "r");
+                if (foundRows.Length > 0)
                 {
-                    totSquares += ((s[0] - rowQ) - 1);
+                    totSquares += ((int.Parse(foundRows[0][0].ToString()) - rowQ) - 1);
                     return totSquares;
                 }
             }
@@ -190,12 +186,7 @@ namespace hackerRank
             return totSquares;
         }
 
-        static bool predBottom(int[] e)
-        {
-            return e[0] > G_rowQ && e[1] == G_colQ;
-        }
-
-        static int calcLeft(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        static int calcLeft(int n, int k, int rowQ, int colQ, DataTable dt)
         {
             if (colQ == 0)
             {
@@ -206,10 +197,10 @@ namespace hackerRank
 
             if (k > 0)
             {
-                s = Array.Find(obstacles, predLeft);
-                if (s != null)
+                DataRow[] foundRows = dt.Select("r = " + rowQ + " and c < " + colQ, "c desc");
+                if (foundRows.Length > 0)
                 {
-                    totSquares += ((colQ - s[1]) - 1);
+                    totSquares += ((colQ - int.Parse(foundRows[0][1].ToString())) - 1);
                     return totSquares;
                 }
             }
@@ -219,12 +210,7 @@ namespace hackerRank
             return totSquares;
         }
 
-        static bool predLeft(int[] e)
-        {
-            return e[0] == G_rowQ && e[1] < G_colQ;
-        }
-
-        static int calcRight(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        static int calcRight(int n, int k, int rowQ, int colQ, DataTable dt)
         {
             if (colQ == (n - 1))
             {
@@ -235,10 +221,10 @@ namespace hackerRank
 
             if (k > 0)
             {
-                s = Array.Find(obstacles, predRight);
-                if (s != null)
+                DataRow[] foundRows = dt.Select("r = " + rowQ + " and c > " + colQ, "c");
+                if (foundRows.Length > 0)
                 {
-                    totSquares += ((s[1] - colQ) - 1);
+                    totSquares += ((int.Parse(foundRows[0][1].ToString()) - colQ) - 1);
                     return totSquares;
                 }
             }
@@ -248,12 +234,7 @@ namespace hackerRank
             return totSquares;
         }
 
-        static bool predRight(int[] e)
-        {
-            return e[0] == G_rowQ && e[1] > G_colQ;
-        }
-
-        static int calcTopLeft(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        static int calcTopLeft(int n, int k, int rowQ, int colQ, DataTable dt)
         {
             if (rowQ == 0 || colQ == 0)
             {
@@ -266,10 +247,11 @@ namespace hackerRank
 
             if (k > 0)
             {
-                s = Array.Find(obstacles, predTopLeft);
-                if (s != null)
+                string op = "r < " + rowQ + " and c < " + colQ + " and (" + colQ + " - c) = (" + rowQ + " - r)";
+                DataRow[] foundRows = dt.Select(op, "r desc, c desc");
+                if (foundRows.Length > 0)
                 {
-                    totSquares += ((rowQ - s[0]) - 1);
+                    totSquares += ((rowQ - int.Parse(foundRows[0][0].ToString())) - 1);
                     return totSquares;
                 }
             }
@@ -293,12 +275,7 @@ namespace hackerRank
             return totSquares;
         }
 
-        static bool predTopLeft(int[] e)
-        {
-            return e[0] < G_rowQ && e[1] < G_colQ && (G_colQ - e[1]) == (G_rowQ - e[0]);
-        }
-
-        static int calcTopRight(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        static int calcTopRight(int n, int k, int rowQ, int colQ, DataTable dt)
         {
             if (rowQ == 0 || colQ == (n - 1))
             {
@@ -312,10 +289,10 @@ namespace hackerRank
 
             if (k > 0)
             {
-                s = Array.Find(obstacles, predTopRight);
-                if (s != null)
+                DataRow[] foundRows = dt.Select("r < " + rowQ + " and c > " + colQ + " and (c - " + colQ + ") = (" + rowQ + " - r)", "r desc, c");
+                if (foundRows.Length > 0)
                 {
-                    totSquares += ((rowQ - s[0]) - 1);
+                    totSquares += ((rowQ - int.Parse(foundRows[0][0].ToString())) - 1);
                     return totSquares;
                 }
             }
@@ -333,12 +310,7 @@ namespace hackerRank
             return totSquares;
         }
 
-        static bool predTopRight(int[] e)
-        {
-            return e[0] < G_rowQ && e[1] > G_colQ && (e[1] - G_colQ) == (G_rowQ - e[0]);
-        }
-
-        static int calcBottomLeft(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        static int calcBottomLeft(int n, int k, int rowQ, int colQ, DataTable dt)
         {
             if (rowQ == (n - 1) || colQ == 0)
             {
@@ -352,10 +324,10 @@ namespace hackerRank
 
             if (k > 0)
             {
-                s = Array.Find(obstacles, predBottomLeft);
-                if (s != null)
+                DataRow[] foundRows = dt.Select("r > " + rowQ + " and c < " + colQ + " and (" + colQ + " - c) = (r - " + rowQ + ")", "r, c desc");
+                if (foundRows.Length > 0)
                 {
-                    totSquares += ((s[0] - rowQ) - 1);
+                    totSquares += ((int.Parse(foundRows[0][0].ToString()) - rowQ) - 1);
                     return totSquares;
                 }
             }
@@ -373,12 +345,7 @@ namespace hackerRank
             return totSquares;
         }
 
-        static bool predBottomLeft(int[] e)
-        {
-            return e[0] > G_rowQ && e[1] < G_colQ && (G_colQ - e[1]) == (e[0] - G_rowQ);
-        }
-
-        static int calcBottomRight(int n, int k, int rowQ, int colQ, int[][] obstacles)
+        static int calcBottomRight(int n, int k, int rowQ, int colQ, DataTable dt)
         {
             if (rowQ == (n - 1) || colQ == (n - 1))
             {
@@ -393,10 +360,10 @@ namespace hackerRank
 
             if (k > 0)
             {
-                s = Array.Find(obstacles, predBottomRight);
-                if (s != null)
+                DataRow[] foundRows = dt.Select("r > " + rowQ + " and c > " + colQ + " and (c - " + colQ + ") = (r - " + rowQ + ")", "r, c");
+                if (foundRows.Length > 0)
                 {
-                    totSquares += ((rowQ - s[0]) - 1);
+                    totSquares += (Math.Abs(rowQ - int.Parse(foundRows[0][0].ToString())) - 1);
                     return totSquares;
                 }
             }
@@ -415,10 +382,47 @@ namespace hackerRank
             return totSquares;
         }
 
+        /*
+        static bool predTop(int[] e)
+        {
+            return e[0] < G_rowQ && e[1] == G_colQ;
+        }
+
+        static bool predBottom(int[] e)
+        {
+            return e[0] > G_rowQ && e[1] == G_colQ;
+        }
+
+        static bool predLeft(int[] e)
+        {
+            return e[0] == G_rowQ && e[1] < G_colQ;
+        }
+
+        static bool predRight(int[] e)
+        {
+            return e[0] == G_rowQ && e[1] > G_colQ;
+        }
+
+        static bool predTopLeft(int[] e)
+        {
+            return e[0] < G_rowQ && e[1] < G_colQ && (G_colQ - e[1]) == (G_rowQ - e[0]);
+        }
+
+        static bool predTopRight(int[] e)
+        {
+            return e[0] < G_rowQ && e[1] > G_colQ && (e[1] - G_colQ) == (G_rowQ - e[0]);
+        }
+
+        static bool predBottomLeft(int[] e)
+        {
+            return e[0] > G_rowQ && e[1] < G_colQ && (G_colQ - e[1]) == (e[0] - G_rowQ);
+        }
+
         static bool predBottomRight(int[] e)
         {
             return e[0] > G_rowQ && e[1] > G_colQ && (e[1] - G_colQ) == (e[0] - G_rowQ);
         }
 
+        */
     }
 }
