e49ed855b42758155bbabfe66fc9e7c59bc18525
riswit <dino.solano@riswit.it>
2019-06-16 20:10:21 +0200

    Travel HackerLand

--- hackerRank/TravelHackerLand.cs
+++ hackerRank/TravelHackerLand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace hackerRank
 {
@@ -26,7 +27,57 @@ namespace hackerRank
             };
             //int resExp = 4;
 
+            int n = 0;
+            int m = 0;
+            int q = 0;
+            int[] S = { };
+
+            string dir = @"F:\test\hr\hackerRank\hackerRank\testTravelHackerLand\";
+            var fileStream = new FileStream(dir + "input01.txt", FileMode.Open, FileAccess.Read);
+
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                string line;
+                int i = 0;
+                int countR = 0;
+                int countQ = 0;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    if (i == 0)
+                    {
+                        S = line.Split(' ').Select(Int32.Parse).ToArray();
+                        n = S[0];
+                        m = S[1];
+                        q = S[2];
+                        t = new int[n];
+                        roads = new int[m][];
+                        queries = new int[q][];
+                    }
+                    else if (i == 1)
+                    {
+                        t = line.Split(' ').Select(Int32.Parse).ToArray();
+                    }
+                    else if (i >= 2 && i < n + 2)
+                    {
+                        roads[countR] = line.Split(' ').Select(Int32.Parse).ToArray();
+                        countR++;
+                    }
+                    else if (i >= n + 2)
+                    {
+                        queries[countQ] = line.Split(' ').Select(Int32.Parse).ToArray();
+                        countQ++;
+                    }
+                    i++;
+                }
+            }
+
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+
             travel(t, roads, queries);
+
+            watch.Stop();
+            Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
         }
 
         static void travel(int[] t, int[][] roads, int[][] queries)
@@ -48,18 +99,15 @@ namespace hackerRank
             }
         }
 
-        static void printAnswerOfQuery(int x, int y, int k, int[] t, int[][] roads, int q, int[][] queries)
+        static void printAnswerOfQuery(int x, int y, int k, int[] t, int[][] roadsOrig, int q, int[][] queries)
         {
-            //List<int> crowds;
-            //int pos = 0;
-            //List<int> numKMet = new List<int>();
-            //int[][] tmp;
-            //int countRoadsFromX = 0;
-            //int countRoadsToX = 0;
-
-            //crowds = new List<int>();
             List<int> listBuild = new List<int>();
             List<int> listCrowd = new List<int>();
+            List<int[]> listPath = new List<int[]>();
+            List<int[]> listCityPathStart = new List<int[]>();
+            List<int[]> listCityPathEnd = new List<int[]>();
+            HashSet<int> hashPathsNumCityAnalyzed = new HashSet<int>();
+            HashSet<int> hashBuild = new HashSet<int>();
             int maxCrowd = 0;
             bool finish = false;
             int min = 0;
@@ -67,48 +115,166 @@ namespace hackerRank
             List<int> numKMet = new List<int>();
             int[][] tmp;
             int totBuildMet = 0;
+            int nBuildMet = 0;
             if (x > y) { min = y; max = x; } else { min = x; max = y; }
             int start = min;
             int end = max;
-            int[] city;
+            int[] r;
+            int tot = 0;
+            int s = 0;
+            int e = 0;
+            int u = 0;
+            int i = 0;
+            int cX = x;
+            int cY = y;
+            int c = 0;
+            bool pathFound = false;
+            int countCycle = 0;
+            int counterListStart = 0;
+            int counterListEnd = 0;
+            int actListAnalyze = 0; // 0: Start - 1: End
 
-            var roadsDest = from array in roads
-                            where (array[1] == end && array[0] < end - 1 )
-                            orderby array[2]
-                            select array;
+            //find path from x to y
 
-            var roadsPath = from array in roads
-                            where (array[0] >= start && array[0] <= end)
-                                && (array[1] >= start && array[1] <= end)
-                                && (array[1] < array[array[1]])
-                            orderby array[2]
-                            select array;
-
-            //listPath = roadsPath.ToList();
-            foreach (int[] road in roadsPath)
+            for (int i1 = 1; i1 <= 2; i1++)
             {
-                //city = new int[2] { t[road[1] - 1], road[0]};
-                listBuild.Add(t[road[1] - 1]);
-                listCrowd.Add(road[2]);
+                if (i1 == 1) { c = cX; counterListStart++; } else { c = cY; counterListEnd++; }
 
+                hashPathsNumCityAnalyzed.Add(c);                
+
+                if (i1 == 1)
+                {
+                    tot = getRoadsOfNumCity(c, k, t, roadsOrig, listBuild, listCrowd, listPath, listCityPathStart);
+                }
+                else
+                {
+                    tot = getRoadsOfNumCity(c, k, t, roadsOrig, listBuild, listCrowd, listPath, listCityPathEnd);
+                }
+
+                if (tot == 0)
+                {
+                    Console.WriteLine("-1");
+                    return;
+                }
             }
 
-            listBuild.Distinct();
-            maxCrowd = listCrowd.Max();
+            nBuildMet = listBuild.Distinct().Count();
+
+            if (existPath(x, y, listCityPathStart) > 0)
+            {
+                pathFound = true;
+            }
+            else if (existPath(x, y, listCityPathEnd) > 0)
+            {
+                pathFound = true;
+            }
+
+            while (counterListStart + counterListEnd <= t.Length && !pathFound)
+            {
+                var roadsTmp = from array in roadsOrig
+                               where (array[0] == c || array[1] == c)
+                               orderby array[2] select array;
+
+
+                totBuildMet += nBuildMet;
+                countCycle += 1;
+            }
+
+            if (counterListStart + counterListEnd >= t.Length && !pathFound)
+            {
+                Console.WriteLine("-1");
+                return;
+            }
+
+
+            //path found, then find better road (road wih min crowd)
+
+
+            //write max Crowd
+
 
 
         }
 
-        //static int[][] getExistPaths(int x, int y, int k, int[] t, int[][] roads)
-        //{
+        static int findRoadsOfity(int c, int k, int[] t, int[][] roadsOrig, List<int> listBuild,
+                            List<int> listCrowd, List<int[]> listPath, List<int[]> listCityPath)
+        {
+            int i = 0;
+            int[] r;
+            int s = 0;
+            int e = 0;
+            int u = 0;
 
-        //    //while (!finish)
-        //    //{
+            var roadsTmp = from array in roadsOrig
+                           where (array[0] == c || array[1] == c)
+                           orderby array[2]
+                           select array;
+            i = 0;
+            foreach (int[] road in roadsTmp)
+            {
+                if (road[0] == c)
+                {
+                    listBuild.Add(t[road[1] - 1]);
+                    e = road[1];
+                }
+                else
+                {
+                    listBuild.Add(t[road[0] - 1]);
+                    e = road[0];
+                }
+                listCrowd.Add(road[2]);
+                //r = new int[2] { c, e };
+                listCityPath.Add(road);
 
-        //    //}
+                i++;
+            }
 
-        //    return listPath.ToArray();
-        //}
+            return i;
+        }
+
+        static int getRoadsOfNumCity(int c, int k, int[] t, int[][] roadsOrig, List<int> listBuild, 
+                                    List<int> listCrowd, List<int[]> listPath, List<int[]> listCityPath)
+        {
+            int i = 0;
+            int[] r;
+            int s = 0;
+            int e = 0;
+            int u = 0;
+
+            var roadsTmp = from array in roadsOrig
+                           where (array[0] == c || array[1] == c)
+                           orderby array[2]
+                           select array;
+            i = 0;
+            foreach (int[] road in roadsTmp)
+            {
+                if (road[0] == c)
+                {
+                    listBuild.Add(t[road[1] - 1]);
+                    e = road[1];
+                }
+                else
+                {
+                    listBuild.Add(t[road[0] - 1]);
+                    e = road[0];
+                }
+                listCrowd.Add(road[2]);
+                //r = new int[2] { c, e };
+                listCityPath.Add(road);
+
+                i++;
+            }
+
+            return i;
+        }
+
+        static int existPath(int a, int b, List<int[]> listCityPath)
+        {
+            var roadsTmp = from array in listCityPath
+                           where (array[0] == a && array[1] == b) || (array[0] == b && array[1] == a)
+                           select array;
+            return roadsTmp.Count();
+        }
 
         static int[][] getRangePathsFromXtoY(int x, int y, int k, int[] t, int[][] roads)
         {
