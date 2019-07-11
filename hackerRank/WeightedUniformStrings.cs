48fde7b0e40ecb52d8536bdf15dbb8d3b14291e1
riswit <dino.solano@riswit.it>
2019-07-11 21:46:14 +0200

    Weighted Uniform Strings

--- hackerRank/WeightedUniformStrings.cs
+++ hackerRank/WeightedUniformStrings.cs
@@ -10,14 +10,14 @@ namespace hackerRank
     {
         public void Execute()
         {
-            string s = "abccddde";
-            int[] queries = { 1, 3, 12, 5, 9, 10 };
-            string[] resExp = { "Yes", "Yes", "Yes", "Yes", "No", "No" };
+            //string s = "abccddde";
+            //int[] queries = { 1, 3, 12, 5, 9, 10 };
+            //string[] resExp = { "Yes", "Yes", "Yes", "Yes", "No", "No" };
 
-            //string s = "aaabbbbcccddd";
-            ////string s = "acaaabzbbbccwcddadaa";
-            //int[] queries = { 9, 7, 8, 12, 5 };
-            //string[] resExp = { "Yes", "No", "Yes", "Yes", "No"};
+            string s = "aaabbbbcccddd";
+            //string s = "acaaabzbbbccwcddadaa";
+            int[] queries = { 9, 7, 8, 12, 5 };
+            string[] resExp = { "Yes", "No", "Yes", "Yes", "No" };
 
             string[] result = weightedUniformStrings(s, queries);
 
@@ -32,107 +32,153 @@ namespace hackerRank
             }
         }
 
-        static bool ctrlDup(char e, int ind, string s)
-        {
-            if (ind == 0)
-            {
-                if (ind < s.Length + 1)
-                {
-                    if (e.Equals(s[ind + 1]))
-                    {
-                        return true;
-                    }
-                    return false;
-                }
-            }
-
-            if (e.Equals(s[ind - 1]))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        static bool ctrlDis(char e, int ind, string s)
-        {
-            if (ind == 0)
-            {
-                return true;
-            }
-
-            if (!e.Equals(s[ind - 1]))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
         static string[] weightedUniformStrings(string s, int[] queries)
+        {
+            string[] res = new string[queries.Length];
+            string a = "abcdefghijklmnopqrstuvwxyz";
+            Dictionary<char, int> dAlph = a.Select((e, ind) => new KeyValuePair<char, int>(e, ind + 1)).ToDictionary(p => p.Key, p => p.Value);
+            int pr = 0;
+            bool[] arrT = new bool[99999999];
+            int w = 0;
+            int totPr = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                w = dAlph[s[i]];
+
+                if (w.Equals(pr))
+                {
+                    arrT[totPr + w] = true;
+                }
+                else
+                {
+                    arrT[w] = true;
+                    totPr = 0;
+                }
+
+                totPr += w;
+                pr = w;
+            }
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                if (arrT[queries[i]])
+                {
+                    res[i] = "Yes";
+                }
+                else
+                {
+                    res[i] = "No";
+                }
+            } 
+
+            return res;
+        }
+
+        static string[] weightedUniformStrings_SLOW(string s, int[] queries)
         {
             string[] res = new string[queries.Length];
 
             string a = "abcdefghijklmnopqrstuvwxyz";
             char[] alp = a.ToCharArray();
 
-            var oDup = s.Where((e, ind) => ctrlDup(e, ind, s));
+            Dictionary<char, int> dAlph = a.Select((e, ind) => new KeyValuePair<char, int>(e, ind + 1)).ToDictionary(p => p.Key, p => p.Value);
 
-            var oDis = s.Where((e, ind) => ctrlDis(e, ind, s)).Distinct();
+            int[] sW = s.Select(e => dAlph[e]).ToArray();
 
-            int[] rdup = oDup.Aggregate(new List<int[]>(), (prev, val) => calc(prev, val, alp)).Select(e => e[1]).ToArray();
-            int[] rdis = oDis.Aggregate(new List<int[]>(), (prev, val) => calc(prev, val, alp)).Select(e => e[1]).ToArray();
+            var oDis = sW.Distinct();
+
+            int[] oDup = sW.Where((e, ind) => ctrlDup(e, ind, sW)).ToArray();
+            int[] oDupR = new int[oDup.Length];
 
             int i = 0;
-            int c = 0;
-            foreach (int q in queries)
+            foreach (int e in oDup)
             {
-                c = Array.Find(rdis, e => e.Equals(q));
-
-                if (c > 0)
+                if (i == 0)
                 {
-                    res[i] = "Yes";
+                    oDupR[i] = oDup[i] * 2;
                 }
                 else
                 {
-                    c = Array.Find(rdup, e => e.Equals(q));
-
-                    if (c > 0)
+                    if (oDup[i].Equals(oDup[i - 1]))
                     {
-                        res[i] = "Yes";
+                        oDupR[i] = oDupR[i - 1] + oDup[i];
                     }
                     else
                     {
-                        res[i] = "No";
+                        oDupR[i] = oDup[i] * 2;
                     }
                 }
+
                 i += 1;
             }
+
+            int[] o = oDis.Concat(oDupR).Distinct().ToArray();
+
+            res = queries.Select(n => o.Contains(n) ? "Yes" : "No").ToArray();
 
             return res;
         }
 
-        static List<int[]> calc(List<int[]> prev, char val, char[] alp)
+        static bool ctrlDup(int e, int ind, int[] sW)
         {
-            int w = 0;
-            w = Array.FindIndex(alp, m => m.Equals(val)) + 1;
-
-            if (prev.Count == 0)
+            if (ind == 0)
             {
-                prev.Add(new int[2] { w, w });
-                return prev;
+                return false;
             }
 
-            if (w.Equals(prev[prev.Count - 1][0]))
+            if (e.Equals(sW[ind - 1]))
             {
-                prev.Add(new int[2] { w, w + prev[prev.Count - 1][1] });
-                return prev;
+                return true;
             }
 
-            prev.Add(new int[2] { w, w });
-
-            return prev;
+            return false;
         }
+
+        //static bool ctrlDis(int e, int ind, int[] sW)
+        //{
+        //    if (ind == 0)
+        //    {
+        //        return true;
+        //    }
+
+        //    if (!e.Equals(sW[ind - 1]))
+        //    {
+        //        if (ind + 1 < sW.Length)
+        //        {
+        //            if (!e.Equals(sW[ind + 1]))
+        //            {
+        //                return true;
+        //            }
+        //            return false;
+        //        }
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
+
+        //static List<int[]> calc(List<int[]> prev, char val, char[] alp)
+        //{
+        //    int w = 0;
+        //    w = Array.FindIndex(alp, m => m.Equals(val)) + 1;
+
+        //    if (prev.Count == 0)
+        //    {
+        //        prev.Add(new int[2] { w, w });
+        //        return prev;
+        //    }
+
+        //    if (w.Equals(prev[prev.Count - 1][0]))
+        //    {
+        //        prev.Add(new int[2] { w, w + prev[prev.Count - 1][1] });
+        //        return prev;
+        //    }
+
+        //    prev.Add(new int[2] { w, w });
+
+        //    return prev;
+        //}
 
     }
 }
