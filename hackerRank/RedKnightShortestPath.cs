6bfc3b9d7d5d401d28826adefbef84845184aca2
riswit <dino.solano@riswit.it>
2019-09-14 20:00:48 +0200

    RedKnightShortestPath

--- hackerRank/RedKnightShortestPath.cs
+++ hackerRank/RedKnightShortestPath.cs
@@ -11,11 +11,30 @@ namespace hackerRank
     {
         public void Execute()
         {
-            int n = 6;
-            int i_start = 6;
-            int i_end = 6;
-            int j_start = 0;
-            int j_end = 1;
+            //int n = 6;
+            //int i_start = 6;
+            //int j_start = 6;
+            //int i_end = 0;
+            //int j_end = 1;
+
+            int n = 70;
+            int i_start = 7;
+            int j_start = 15;
+            int i_end = 67;
+            int j_end = 3;
+
+            //int n = 100;
+            //int i_start = 2;
+            //int j_start = 24;
+            //int i_end = 92;
+            //int j_end = 45;
+
+            //int n = 150;
+            //int i_start = 24;
+            //int j_start = 15;
+            //int i_end = 46;
+            //int j_end = 102;
+
             printShortestPath(n, i_start, j_start, i_end, j_end);
         }
 
@@ -62,12 +81,16 @@ namespace hackerRank
 
             while (!d.Equals(Direction.Center))
             {
-                arrMove = moveK(arrMove, d, r_end, c_end);
-                if (arrMove[2] == 1)
+                arrMove = moveK(n, arrMove, d, r_end, c_end);
+
+                if (arrMove[2] == -1)
                 {
                     Console.WriteLine("Impossible");
                     return;
                 }
+
+                d = (Direction)Enum.Parse(typeof(Direction), arrMove[2].ToString());
+
                 if (d.Equals(Direction.UpLeft)
                     || d.Equals(Direction.UpRight)
                     || d.Equals(Direction.Right)
@@ -75,6 +98,21 @@ namespace hackerRank
                     || d.Equals(Direction.LowerLeft)
                     || d.Equals(Direction.Left))
                 {
+                    int cS = listSteps.Count();
+
+                    if (cS > 0 && (d.Equals(Direction.Left) || d.Equals(Direction.Right)))
+                    {
+                        if (d.Equals(Direction.Left) && listSteps[cS - 1] == "R")
+                        {
+                            Console.WriteLine("Impossible");
+                            return;
+                        }
+                        if (d.Equals(Direction.Right) && listSteps[cS - 1] == "L")
+                        {
+                            Console.WriteLine("Impossible");
+                            return;
+                        }
+                    }
                     listSteps.Add(dict[(int)d]);
                 }
                 d = getDirection(n, arrMove[0], arrMove[1], r_end, c_end);
@@ -87,17 +125,13 @@ namespace hackerRank
                 if (arrMove[0] == r_end && arrMove[1] == c_end)
                 {
                     d = Direction.Center;
-
-                    roads.Add(nRoad, listSteps);
-                    arrMove[0] = r_start;
-                    arrMove[1] = c_start;
-                    arrMove[2] = 0;
-
-                    listSteps = new List<string>();
-                    d = getDirection(n, r_start, c_start, r_end, c_end);
-
                     nRoad += 1;
                 }
+            }
+
+            if (verifyLRLL(listSteps))
+            {
+                listSteps = listSteps.OrderByDescending(x => x).ToList();
             }
 
             Console.WriteLine(listSteps.Count());
@@ -107,7 +141,7 @@ namespace hackerRank
             }
         }
 
-        static int[] moveK(int[] arrMove, Direction d, int r_end, int c_end)
+        static int[] moveK(int n, int[] arrMove, Direction d, int r_end, int c_end)
         {
             if (d.Equals(Direction.UpLeft))
             {
@@ -115,51 +149,136 @@ namespace hackerRank
                 {
                     arrMove[1] -= 1;
                     arrMove[0] -= 2;
-                    arrMove[2] = 0;
+                    arrMove[2] = 1;
                 }
                 else
                 {
-                    arrMove[2] = 1;
+                    arrMove[2] = -1;
                 }
             }
             else if (d.Equals(Direction.Up))
             {
-
+                if (arrMove[1] - 1 >= 0 && arrMove[0] - 2 >= 0)
+                {
+                    arrMove[1] -= 1;
+                    arrMove[0] -= 2;
+                    arrMove[2] = 1;
+                }
+                else if (arrMove[1] + 1 < n && arrMove[0] - 2 >= 0)
+                {
+                    arrMove[1] += 1;
+                    arrMove[0] -= 2;
+                    arrMove[2] = 3;
+                }
+                else
+                {
+                    arrMove[2] = -1;
+                }
             }
             else if (d.Equals(Direction.UpRight))
             {
-
+                if (arrMove[1] + 1 < n && arrMove[0] - 2 >= 0)
+                {
+                    arrMove[1] += 1;
+                    arrMove[0] -= 2;
+                    arrMove[2] = 3;
+                }
+                else
+                {
+                    arrMove[2] = -1;
+                }
             }
             else if (d.Equals(Direction.Right))
             {
-
+                if (arrMove[1] + 2 < n)
+                {
+                    arrMove[1] += 2;
+                    arrMove[2] = 4;
+                }
+                else
+                {
+                    arrMove[2] = -1;
+                }
             }
             else if (d.Equals(Direction.LowerRight))
             {
+                int sR = (r_end - arrMove[0]) / 2;
+                int sC = c_end - arrMove[1];
 
+                if (arrMove[1] + 2 < n && sC - sR > 0)
+                {
+                    arrMove[1] += 2;
+                    arrMove[2] = 4;
+                }
+                else if (arrMove[1] + 1 < n && arrMove[0] + 2 < n)
+                {
+                    arrMove[1] += 1;
+                    arrMove[0] += 2;
+                    arrMove[2] = 5;
+                }
+                else
+                {
+                    arrMove[2] = -1;
+                }
             }
             else if (d.Equals(Direction.Down))
             {
-
+                if (arrMove[1] + 1 < n && arrMove[0] + 2 < n)
+                {
+                    arrMove[1] += 1;
+                    arrMove[0] += 2;
+                    arrMove[2] = 5;
+                }
+                else if (arrMove[1] - 1 >= 0 && arrMove[0] + 2 < n)
+                {
+                    arrMove[1] -= 1;
+                    arrMove[0] += 2;
+                    arrMove[2] = 7;
+                }
+                else
+                {
+                    arrMove[2] = -1;
+                }
             }
             else if (d.Equals(Direction.LowerLeft))
             {
-
+                if (arrMove[1] - 1 >= 0 && arrMove[0] + 2 < n)
+                {
+                    arrMove[1] -= 1;
+                    arrMove[0] += 2;
+                    arrMove[2] = 7;
+                }
+                else
+                {
+                    arrMove[2] = -1;
+                }
             }
             else if (d.Equals(Direction.Left))
             {
                 if (arrMove[1] - 2 >= 0)
                 {
                     arrMove[1] -= 2;
-                    arrMove[2] = 0;
+                    arrMove[2] = 8;
                 }
                 else
                 {
-                    arrMove[2] = 1;
+                    arrMove[2] = -1;
                 }
             }
 
             return arrMove;
+        }
+
+        static bool verifyLRLL(List<string> listSteps)
+        {
+            foreach (string e in listSteps)
+            {
+                if (!e.Equals("LL") && !e.Equals("LR"))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         static Direction getDirection(int n, int r_start, int c_start, int r_end, int c_end)
